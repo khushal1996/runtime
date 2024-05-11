b55f3eac363924f08bc838745451b71124289c3f
Khushal Modi <kcmodi@asu.edu>
2024-05-10 20:18:35 -0700

    Lower AVX10v1 hwintrinsic in lowering and gentree.cpp for simdSize 32/16

--- src/coreclr/jit/simdashwintrinsic.cpp
+++ src/coreclr/jit/simdashwintrinsic.cpp
@@ -547,7 +547,8 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
 #if defined(TARGET_XARCH)
         case NI_VectorT_ConvertToDouble:
         {
-            if (IsBaselineVector512IsaSupportedOpportunistically())
+            if (IsBaselineVector512IsaSupportedOpportunistically() ||
+                ((simdSize != 64) && compOpportunisticallyDependsOn(InstructionSet_AVX10v1)))
             {
                 break;
             }
@@ -567,7 +568,8 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
         case NI_VectorT_ConvertToUInt32:
         case NI_VectorT_ConvertToUInt64:
         {
-            if (IsBaselineVector512IsaSupportedOpportunistically())
+            if (IsBaselineVector512IsaSupportedOpportunistically() ||
+                (simdSize != 64 && compOpportunisticallyDependsOn(InstructionSet_AVX10v1)))
             {
                 break;
             }
@@ -577,7 +579,9 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
         case NI_VectorT_ConvertToSingle:
         {
             if ((simdBaseType == TYP_INT) ||
-                (simdBaseType == TYP_UINT && IsBaselineVector512IsaSupportedOpportunistically()))
+                (simdBaseType == TYP_UINT &&
+                 (IsBaselineVector512IsaSupportedOpportunistically() ||
+                  (simdSize != 64 && compOpportunisticallyDependsOn(InstructionSet_AVX10v1)))))
             {
                 break;
             }
@@ -1211,18 +1215,33 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
                     assert(sig->numArgs == 1);
                     assert(varTypeIsLong(simdBaseType));
                     NamedIntrinsic intrinsic = NI_Illegal;
-                    if (simdSize == 64)
+                    if (compOpportunisticallyDependsOn(InstructionSet_AVX10v1))
                     {
-                        intrinsic = NI_AVX512DQ_ConvertToVector512Double;
-                    }
-                    else if (simdSize == 32)
-                    {
-                        intrinsic = NI_AVX512DQ_VL_ConvertToVector256Double;
+                        if (simdSize == 32)
+                        {
+                            intrinsic = NI_AVX10v1_ConvertToVector256Double;
+                        }
+                        else
+                        {
+                            assert(simdSize == 16);
+                            intrinsic = NI_AVX10v1_ConvertToVector128Double;
+                        }
                     }
                     else
                     {
-                        assert(simdSize == 16);
-                        intrinsic = NI_AVX512DQ_VL_ConvertToVector128Double;
+                        if (simdSize == 64)
+                        {
+                            intrinsic = NI_AVX512DQ_ConvertToVector512Double;
+                        }
+                        else if (simdSize == 32)
+                        {
+                            intrinsic = NI_AVX512DQ_VL_ConvertToVector256Double;
+                        }
+                        else
+                        {
+                            assert(simdSize == 16);
+                            intrinsic = NI_AVX512DQ_VL_ConvertToVector128Double;
+                        }
                     }
                     return gtNewSimdHWIntrinsicNode(retType, op1, intrinsic, simdBaseJitType, simdSize);
                 }
@@ -1243,6 +1262,21 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
                                 break;
                             case 64:
                                 intrinsic = NI_AVX512F_ConvertToVector512Single;
+                                break;
+                            default:
+                                unreached();
+                        }
+                    }
+                    else if (simdBaseType == TYP_UINT && simdSize != 64 &&
+                             compOpportunisticallyDependsOn(InstructionSet_AVX10v1))
+                    {
+                        switch (simdSize)
+                        {
+                            case 16:
+                                intrinsic = NI_AVX10v1_ConvertToVector128Single;
+                                break;
+                            case 32:
+                                intrinsic = NI_AVX10v1_ConvertToVector256Single;
                                 break;
                             default:
                                 unreached();
