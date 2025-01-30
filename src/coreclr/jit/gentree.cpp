0338a8f419eb7eeb1adb76279ac6e192aa0ef6be
Khushal Modi <kcmodi@asu.edu>
2025-01-30 15:33:37 -0800

    resolve compile errors

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -24680,9 +24680,6 @@ GenTree* Compiler::gtNewSimdMinNode(
  * @return A new GenTree node representing the SIMD min/max operation.
  */
 GenTree* Compiler::gtNewSimdMinMaxNode(
-    var_types type, GenTree* op1, GenTree* op2, ssize_t ctrlByte, CorInfoType simdBaseJitType, unsigned simdSize);
-
-GenTree* Compiler::gtNewSimdMinMaxNode(
     var_types type, GenTree* op1, GenTree* op2, ssize_t ctrlByte, CorInfoType simdBaseJitType, unsigned simdSize)
 {
     assert(IsBaselineSimdIsaSupportedDebugOnly());
