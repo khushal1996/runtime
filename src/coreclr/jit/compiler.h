db6cab7798fcd7b1c581e98fcf54c8ada6d9d3db
Khushal Modi <kcmodi@asu.edu>
2025-03-13 13:53:07 -0700

    Add APX bypass Knob

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -9710,6 +9710,12 @@ public:
     //
     bool canUseApxEncoding() const
     {
+#ifdef DEBUG
+        if (JitConfig.JitBypassApxCheck())
+        {
+            return true;
+        }
+#endif
         return compOpportunisticallyDependsOn(InstructionSet_APX);
     }
 
