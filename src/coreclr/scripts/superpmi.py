1af32e9e38f3e601b097033d30b71b23f495483b
Ruihan-Yin <ruihan.yin@intel.com>
2025-05-02 14:27:08 -0700

    Update Superpmi.py to collect icount metrics

--- src/coreclr/scripts/superpmi.py
+++ src/coreclr/scripts/superpmi.py
@@ -2716,12 +2716,8 @@ class SuperPMIReplayAsmDiffs:
             smallest_contexts = sorted(diffs, key=lambda r: int(r["Context size"]))[:20]
             display_subset("Smallest {} contexts with binary differences:", smallest_contexts)
 
-            if self.coreclr_args.metrics is None:
-                base_metric_name = "Base ActualCodeBytes"
-                diff_metric_name = "Diff ActualCodeBytes"
-            else:
-                base_metric_name = "Base PerfScore"
-                diff_metric_name = "Diff PerfScore"
+            base_metric_name = "Base InstructionCount"
+            diff_metric_name = "Diff InstructionCount"
 
             # Order by improvement, largest improvements first
             by_diff = sorted(diffs, key=lambda r: float(r[diff_metric_name]) - float(r[base_metric_name]))
@@ -2757,7 +2753,7 @@ class SuperPMIReplayAsmDiffs:
             # Prefer to show small diffs over large percentage wise diffs; sort by this additionally.
             # sorted is stable, so for multiple small diffs this will keep them in order of percentage wise improvement/regression.
             def is_small_diff(row):
-                if abs(int(row['Diff ActualCodeBytes']) - int(row['Base ActualCodeBytes'])) < 50:
+                if abs(int(row[diff_metric_name]) - int(row[base_metric_name])) < 50:
                     return 0
 
                 return 1
@@ -3025,8 +3021,8 @@ def write_example_diffs_to_markdown_summary(write_fh, asm_diffs):
 
             with DetailsSection(write_fh, collection_name):
                 for (func_name, diff, diff_text) in examples_to_put_in_summary:
-                    base_size = int(diff["Base ActualCodeBytes"])
-                    diff_size = int(diff["Diff ActualCodeBytes"])
+                    base_size = int(diff["Base InstructionCount"])
+                    diff_size = int(diff["Diff InstructionCount"])
                     with DetailsSection(write_fh, "{} ({}) : {}".format(format_delta(base_size, diff_size), compute_and_format_pct(base_size, diff_size), func_name)):
                         write_fh.write(diff_text)
 
