9f6822cf657693b884c829ed85729a105c0d8b3a
Khushal Modi <kcmodi@asu.edu>
2025-10-15 19:31:16 -0700

    Enable CFCMOVcc instructions in JIT emitter for APX

--- src/coreclr/jit/codegen.h
+++ src/coreclr/jit/codegen.h
@@ -607,6 +607,7 @@ protected:
     void genAmd64EmitterUnitTestsApx();
     void genAmd64EmitterUnitTestsAvx10v2();
     void genAmd64EmitterUnitTestsCCMP();
+    void genAmd64EmitterUnitTestsCFCMOV();
 #endif
 
 #endif // defined(DEBUG)
